75eaeed824a295bb0666f93eabacfd3c4c5f5f53
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2020-05-27 13:32:19 +0600

    cifar-10 added

---
+++ Linear Regression Simulation using Tkinter/main.py
@@ -0,0 +1,119 @@
+from tkinter import *
+import random
+
+# intializing canvas width & height
+width = 600
+height = 600
+
+# initializing weight, bias & learning rate
+m = 1
+b = 0
+lr = 0.0
+
+# update learning rate respect to scale
+def setLR(val):
+    global lr
+    lr = float(val)
+
+# map function for normalization & coordinate conversion
+def map(n, start1, stop1, start2, stop2):
+    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2
+
+# reset the canvas data [reset Button]
+def reset():
+    points.clear()
+    drawing_area.delete("all")
+    m = 1
+    b = 0
+
+train = False
+def startTrain():
+    global train
+    if train == True:
+        train = False
+        startBtn['text'] = "Start Training"
+        startBtn['background'] = "#5bc0de"
+    else:
+        train = True
+        startBtn['text'] = "Stop Training"
+        startBtn['background'] = "#f0ad4e"
+
+
+# Draw points in the canvas
+points = []
+def drawPoint(event):
+    x = event.x
+    y = event.y
+
+    x1, y1 = (x - 1), (y + 1)
+    x2, y2 = (x + 1), (y - 1)
+    drawing_area.create_oval(x1, y1, x2, y2, fill="black")
+
+    x = map(x, 0, width, 0, 1)
+    y = map(y, 0, height, 1, 0)
+    points.append((x, y))
+
+# Gradient Descent Algorithm for loss/error optimization
+def gradientDescent():
+    global m
+    global b
+    for x, y in points:
+        Y = m*x+b
+        error = Y-y
+        m = m-(error*x*lr)
+        b = b-(error*lr)
+    eqnLabel['text'] = "y = "+ str(round(m,2))+"x+"+str(round(b,2))
+
+
+# Draw linear line 
+lineId = None
+def drawLine():
+    global lineId
+    if len(points) >= 2:
+        if train == True:
+            gradientDescent()
+
+        x1 = 0
+        x2 = 1
+        y1 = m*x1+b
+        y2 = m*x2+b
+
+        x1 = map(x1, 0, 1, 0, width)
+        y1 = map(y1, 0, 1, height, 0)
+        x2 = map(x2, 0, 1, 0, width)
+        y2 = map(y2, 0, 1, height, 0)
+
+        if lineId is not None:
+            drawing_area.delete(lineId)
+        lineId = drawing_area.create_line(x1, y1, x2, y2)
+
+    drawing_area.after(100, drawLine)
+
+
+
+root = Tk()
+root.title("Linear Regression")
+
+header = Frame(root)
+header.pack(side=TOP)
+
+lrScale = Scale(header, orient=HORIZONTAL, length=400, from_=0, to=1, resolution=0.01, label="Learning Rate", command=setLR)
+lrScale.grid(row=0, column=0, rowspan=2)
+
+resetBtn = Button(header, text="Reset", background="#d9534f", command=reset)
+resetBtn.grid(row=0, column=1, padx=50)
+
+startBtn = Button(header, text="Start Training", background="#5bc0de", command=startTrain)
+startBtn.grid(row=1, column=1, padx=50)
+
+eqnLabel = Label(header, text="y = "+ str(m)+"x+"+str(b))
+eqnLabel.grid(row=2, column=0)
+
+drawing_area = Canvas(root, width=width, height=height,background="white")
+drawing_area.bind("<Button-1>", drawPoint)
+drawing_area.pack()
+
+
+drawLine()
+
+root.mainloop()
